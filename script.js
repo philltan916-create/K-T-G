64c6a7182578b9763a6020b4662ae580fa3ce64c
philltan916-create <philltan916@gmail.com>
2026-03-15 18:18:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const cats = [
+    {
+        name: "Jeremy",
+        quote: "Please stop looking at your phone and pet me.",
+        img: "cat1.jpg"
+    },
+    {
+        name: "Mittens",
+        quote: "I sleep on your keyboard when you work.",
+        img: "cat2.jpg"
+    },
+    {
+        name: "Whiskers",
+        quote: "I knocked over your cup but it's okay, love me.",
+        img: "cat3.jpg"
+    }
+];
+
+function displayCatOfTheWeek() {
+    const randomCat = cats[Math.floor(Math.random() * cats.length)];
+    document.getElementById("cat-img").src = randomCat.img;
+    document.getElementById("cat-quote").textContent = randomCat.quote;
+    document.getElementById("cat-name").textContent = `– ${randomCat.name}`;
+}
+
+
+window.onload = displayCatOfTheWeek;
